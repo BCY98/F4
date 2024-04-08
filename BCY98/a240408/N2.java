e4aba7f45acdd6d0c546a56c32d7251bce786bac
BCY <gkfnal0120@naver.com>
2024-04-08 17:33:09 +0900

    0408코딩테스트(4)

--- BCY98/a240408/N2.java
+++ BCY98/a240408/N2.java
@@ -2,25 +2,24 @@ package BCY98.a240402;
 
 public class N2 {
     public int[] solution(int n, long left, long right) {
-        int r = Long.valueOf(right).intValue();
-        int l = Long.valueOf(left).intValue();
-        int size = (r - l + 1);
+        int size = (int) (right - left + 1);
         int[] answer = new int[size];
 
-        int index = 0;
-        for (int i = l; i <= r; i++) {
-            int row = (int) (i / n);
-            int col = (int) (i % n); 
+        int idx = 0;
+        for (long i = left; i <= right; i++) {
+            int x = (int) (i / n);
+            int y = (int) (i % n);
 
-            
-            answer[index++] = Math.max(row, col) + 1;
+            // 주어진 행과 열에 해당하는 값 구하기
+            answer[idx++] = Math.max(x, y) + 1;
         }
+
         return answer;
     }
 
-//    public static void main(String[] args) {
-//        N2 n2 = new N2();
-//        System.out.println(n2.solution(3,2,5));
-//
-//    }
+    public static void main(String[] args) {
+        N2 n2 = new N2();
+        System.out.println(n2.solution(3,2,5));
+
+    }
 }
